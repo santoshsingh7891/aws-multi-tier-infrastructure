9428303366f91b57fb64c2d6ae0919976c611b68
santoshsingh7891 <singh.santosh7891@gmail.com>
2026-05-17 15:56:17 +0530

    Add multiple variables for AWS configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,3 +1,35 @@
 variable "aws_region" {
   default = "us-east-1"
 }
+
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet" {
+  default = "10.0.1.0/24"
+}
+
+variable "private_subnet" {
+  default = "10.0.2.0/24"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  default = "SantoshNewKey"
+}
+
+variable "db_name" {
+  default = "intel"
+}
+
+variable "db_username" {
+  default = "admin"
+}
+
+variable "db_password" {
+  default = "intel123"
+}
