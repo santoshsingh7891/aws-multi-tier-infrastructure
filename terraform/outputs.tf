e0685f29ce123b132a819aa6506f7b8538b7b49c
santoshsingh7891 <singh.santosh7891@gmail.com>
2026-05-17 16:00:33 +0530

    Add outputs for EC2 public IP and RDS endpoint

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
-output "aws_region" {
-  value = var.aws_region
+output "ec2_public_ip" {
+  value = aws_instance.web.public_ip
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.mysql.endpoint
 }
