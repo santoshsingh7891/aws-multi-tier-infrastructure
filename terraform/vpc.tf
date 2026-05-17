192e8675ecb9df511260f0cbe9401d64a33402c9
santoshsingh7891 <singh.santosh7891@gmail.com>
2026-05-17 15:56:49 +0530

    Add VPC and Internet Gateway resources

---
+++ terraform/vpc.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+
+  tags = {
+    Name = "project-vpc"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "project-igw"
+  }
+}
